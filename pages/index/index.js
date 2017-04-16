fcac114298ffd33dd5a12b8b87ad0e8f34af73e4
guonima <504117608@qq.com>
2017-04-16 22:52:38 +0800

    增加首页

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,13 +4,39 @@ var httpClient = require('../../utils/httpClient.js');
 var app = getApp();
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {}
+    flag : ""
   },
   //事件处理函数
-  bindViewTap: function () {
-    wx.navigateTo({
-      url: '../logs/logs'
+  selectPrintPicture1: function () {
+    this.data.flag = 1;
+    this.selectPrintPicture();
+  },
+  selectPrintPicture2: function () {
+    this.data.flag = 2;    
+    this.selectPrintPicture();
+  },
+  selectPrintPicture3: function () {
+    this.data.flag = 3;    
+    this.selectPrintPicture();
+  },
+  selectPrintPicture4: function () {
+    this.data.flag = 4;    
+    this.selectPrintPicture();
+  },
+  selectPrintPicture: function () {
+    wx.chooseImage({
+      count: 5, // 默认9
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success: function (res) {
+        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+        console.log(res);
+        var tempFilePaths = res.tempFilePaths;
+        wx.previewImage({
+          current: tempFilePaths[0], // 当前显示图片的http链接
+          urls: tempFilePaths // 需要预览的图片http链接列表
+        })
+      }
     })
   },
   onLoad: function () {
