a7c11036e72aa1818f75913d60586ef5772ed25b
guonima <504117608@qq.com>
2017-06-05 07:53:40 +0800

    支付模块编写

--- pages/views/order/details/details.js
+++ pages/views/order/details/details.js
@@ -4,20 +4,30 @@ var util = require('../../../../utils/util.js');
 var app = getApp();
 
 Page({
-    data: {
-        detail: [],
-        serverUrl: app.globalParam.serverUrl
-    },
+  data: {
+    detail: [],
+    serverUrl: app.globalParam.serverUrl
+  },
 
-    onLoad: function (option) {
-        var orderNo = option.orderNo; // 订单号
-        var that = this;
-        var url = "/orders/" + orderNo;
-        httpClient.request(url, {}, "GET",
-            function (response) {
-                that.setData({ detail: response });
-            }
-        );
-    },
+  onLoad: function (option) {
+    var orderNo = option.orderNo; // 订单号
+    var that = this;
+    var url = "/orders/" + orderNo;
+    httpClient.request(url, {}, "GET",
+      function (response) {
+        that.setData({ detail: response });
+      }
+    );
+  },
+
+  paymentOrder: function (event) {
+    var orderNo = event.currentTarget.dataset.orderNo;
+    var url = "/orders/" + orderNo;    
+    httpClient.request(url, {}, "GET",
+      function (response) {
+        that.setData({ detail: response });
+      }
+    );
+  },
 
 });
